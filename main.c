ae8fead8f674076700d8e24940e12c9b5a01f9e2
bhanu-ni <59286466+bhanu-ni@users.noreply.github.com>
2022-08-09 17:10:29 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,34 +2,27 @@
 #include <math.h>
 int main()
 {
-    int a, b, c, d, e, r1, r2, real, complex;
+    int a, b, c;
+    float d, r1, r2, real, complex;
     printf("Enter coefficients a, b and c: ");
     scanf("%d %d %d", &a, &b, &c);
     d = b * b - 4 * a * c;
-    e = -b + sqrt(d);
     if (a != 0)
     {
         // For discriminant grater than zero
         // or roots are real
         if (d > 0)
         {
-            if (e % 2 * a == 0)
-            {
-                r1 = (-b + sqrt(d)) / (2 * a);
-                r2 = (-b - sqrt(d)) / (2 * a);
-                printf("r1: %d \nr2: %d", r1, r2);
-            }
-            else
-            {
-                printf("r1: %d / %d", e, 2 * a);
-            }
+            r1 = (-b + sqrt(d)) / (2 * a);
+            r2 = (-b - sqrt(d)) / (2 * a);
+            printf("r1: %.2g \nr2: %.2g", r1, r2);
         }
         // For discriminant equale to zero
         // or both the roots are same
         else if (d == 0)
         {
             r1 = -b / (2 * a);
-            printf("r1 = r2: %d", r1);
+            printf("r1 = r2: %.2g", r1);
         }
 
         // For discriminant smaller than zero
@@ -40,17 +33,17 @@ int main()
             complex = sqrt(-d) / (2 * a);
             if (-b % 2 * a == 0)
             {
-                printf("r1: %d + %di\nr2: %d - %di", real, complex, real, complex);
+                printf("r1: %.2g + %.2gi\nr2: %.2g - %.2gi", real, complex, real, complex);
             }
             else
             {
-                printf("r1: %d/%d + √%di\nr2: %d/%d - √%di", -b, 2 * a, -d, -b, 2 * a, -d);
+                printf("r1: %d/%d + %c%.2g/%di\nr2: %d/%d - %c%.2g/%di", -b, 2 * a, 251, -d, 2 * a, -b, 2 * a, 251, -d, 2 * a);
             }
         }
     }
     else
     {
-        printf("Wrong input a can't be zero!");
+        printf("Wrong input 'a' can't be zero!");
     }
 
     return 0;
